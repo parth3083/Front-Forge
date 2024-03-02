40588f7fbb911746020e5dd9a704ab353864896b
parth3083 <parthbrajput30@gmail.com>
2024-03-02 15:31:32 +0530

    started project

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+
+function App() {
+  return (
+    <>
+      <Button>Get Started</Button>
+    </>
+  )
+}
+
+export default App
